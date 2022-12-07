998298cfce627b3098c9063a9d7bc2056c500817
Kyle Kirkby <kyle.kirkby@linaro.org>
2022-12-07 11:11:04 +0000

    Configuring jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {};
 module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
   roots: ["src"],
   setupFilesAfterEnv: ["./jest.setup.ts"],
   moduleFileExtensions: ["ts", "tsx", "js"],
   testPathIgnorePatterns: ["node_modules/"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.js?$": "babel-jest",
   },
   testMatch: ["**/*.test.(ts|tsx)"],
   moduleNameMapper: {
@@ -12,5 +17,7 @@ module.exports = {
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "identity-obj-proxy",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "components(.*)$": "<rootDir>/src/components/$1",
+    "^lib/(.*)$": "<rootDir>/src/lib/$1",
   },
 };
