903308a13234a22bfc0bf70e624a5684de9f7bfc
Kyle Kirkby <kyle.kirkby@linaro.org>
2022-12-16 11:17:47 +0000

    Updated story / test

--- src/components/DrawerLinks/DrawerLinks.stories.tsx
+++ src/components/DrawerLinks/DrawerLinks.stories.tsx
@@ -1,9 +1,180 @@
 // Generated with util/create-component.js
-  import React from "react";
-  import DrawerLinks from "./DrawerLinks";
-  export default {
-      title: "DrawerLinks"
-  };
-  export const WithBar = () => <DrawerLinks foo="bar" />;
-  export const WithBaz = () => <DrawerLinks foo="baz" />;
-  
+import React from "react";
+import DrawerLinks from "./DrawerLinks";
+export default {
+  title: "DrawerLinks",
+};
+const examplePages = [
+  {
+    title: "Solutions",
+    subMenus: [
+      {
+        title: "Automotive, IoT & Edge Devices",
+        pathname: "/automotive-iot-and-edge-devices/",
+      },
+      {
+        title: "Client Devices",
+        pathname: "/client-devices/",
+      },
+      {
+        title: "Cloud Computing & Servers",
+        pathname: "/cloud-computing-and-servers/",
+      },
+
+      {
+        title: "Overview",
+        pathname: "/core-technologies/",
+      },
+      {
+        title: "Artificial Intelligence",
+        pathname: "/core-technologies/artificial-intelligence/",
+      },
+      {
+        title: "Linux Kernel",
+        pathname: "/core-technologies/linux-kernel/",
+      },
+      {
+        title: "Security",
+        pathname: "/core-technologies/security/",
+      },
+      {
+        title: "Testing & CI",
+        pathname: "/core-technologies/testing-and-ci/",
+      },
+      {
+        title: "Toolchain",
+        pathname: "/core-technologies/toolchain/",
+      },
+      {
+        title: "Virtualization",
+        pathname: "/core-technologies/virtualization/",
+      },
+    ],
+  },
+  {
+    title: "Membership",
+    subMenus: [
+      {
+        title: "Membership Overview",
+        pathname: "/membership/",
+      },
+      {
+        title: "Group Membership",
+        pathname: "/membership/groups/",
+      },
+      {
+        title: "Projects",
+        pathname: "/projects/",
+      },
+      {
+        title: "Windows on Arm Group",
+        pathname: "/windows-on-arm/",
+      },
+      {
+        title: "Community Projects",
+        pathname: "/community-projects/",
+      },
+    ],
+  },
+  {
+    title: "Services",
+    subMenus: [
+      {
+        title: "Overview",
+        pathname: "/services/",
+      },
+      {
+        title: "Hands on training",
+        pathname: "/services/hands-on-training/",
+      },
+      {
+        title: "Security",
+        pathname: "/services/security/",
+      },
+      {
+        title: "Testing & Long term support",
+        pathname: "/services/testing-and-long-term-support/",
+      },
+      {
+        title: "Board Support Packages",
+        pathname: "/services/board-support-packages/",
+      },
+      {
+        title: "System Performance & Optimization",
+        pathname: "/services/system-performance-and-optimization/",
+      },
+      {
+        title: "Qualcomm Platform Services",
+        pathname: "/services/qualcomm-platforms-services/",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    subMenus: [
+      {
+        title: "Downloads",
+        pathname: "/downloads/",
+      },
+      {
+        title: "Whitepapers",
+        pathname: "/whitepapers/",
+      },
+      {
+        title: "Learning Hub",
+        pathname: "/learning-hub/",
+      },
+      {
+        title: "Linaro Resources Hub",
+        pathname: "https://resources.linaro.org",
+      },
+    ],
+  },
+  {
+    title: "Support",
+    pathname: "/support/",
+  },
+  {
+    title: "About",
+    subMenus: [
+      {
+        title: "About Linaro",
+        pathname: "/about/",
+      },
+      {
+        title: "Team",
+        pathname: "/about/team/",
+      },
+      {
+        title: "Technical Steering Committee",
+        pathname: "/tsc/",
+      },
+      {
+        title: "Contact us",
+        pathname: "/contact/",
+      },
+      {
+        title: "Linaro Connect",
+        pathname: "/connect/",
+      },
+      {
+        title: "Careers",
+        pathname: "/careers/",
+      },
+      {
+        title: "Blogs",
+        pathname: "/blog/",
+      },
+      {
+        title: "News",
+        pathname: "/news/",
+      },
+      {
+        title: "Events",
+        pathname: "/events/",
+      },
+    ],
+  },
+];
+export const WithBar = () => <DrawerLinks pages={examplePages} />;
+export const WithBaz = () => <DrawerLinks pages={examplePages} />;
