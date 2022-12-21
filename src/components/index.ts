117304eaaab2630e426ccb3aad66f4829c43126c
Kyle Kirkby <kyle.kirkby@linaro.org>
2022-12-21 14:07:31 +0000

    Adding snackbar provider export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./NavBar";
+export { default as CustomSnackbarProvider } from "./CustomSnackbarProvider";
 export { default as Seo } from "./Seo";
 export { default as Linked } from "./Linked";
